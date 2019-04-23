7e48d8d1facea23d0f4ee4642dc103fc2c748ffc
san <ghaisanyukta@seattleu.edu>
2019-04-23 10:59:59 -0700

    Add customer json and xml files

---
+++ custdata.js
@@ -0,0 +1,124 @@
+{
+  "data": [
+     {
+      "name": "James Bond",
+      "company": "Secret Service",
+      "emailid": "jamesbond@secretservice.com",
+      "contactno": "123-456-999",
+      "starting_date": "2017/07/25",
+      "location": "Seattle"
+   },
+   {
+      "name": "Tim Cook",
+      "company": "Apple Inc",
+      "emailid": "timcook@apple.com",
+      "contactno": "123-456-888",
+      "starting_date": "2017/01/01",
+      "location": "Cupertino"
+   },
+   {
+      "name": "Jeff Bezos",
+      "company": "Amazon",
+      "emailid": "jeffb@amazon.com",
+      "contactno": "123-456-777",
+      "starting_date": "2017/10/01",
+      "location": "Seattle"
+   },
+   {
+      "name": "Steve Jobs",
+      "company": "Apple Inc",
+      "emailid": "stevejobs@apple.com",
+      "contactno": "123-456-666",
+      "starting_date": "2009/07/25",
+      "location": "Cupertino"
+   },
+   {
+      "name": "Elon Musk",
+      "company": "Tesla",
+      "emailid": "elonmusk@tesla.com",
+      "contactno": "123-456-555",
+      "starting_date": "2015/01/01",
+      "location": "Palo Alto"
+   },
+   {
+      "name": "Bill Gates",
+      "company": "Microsoft",
+      "emailid": "billgates@microsoft.com",
+      "contactno": "123-456-444",
+      "starting_date": "2008/10/03",
+      "location": "Seattle"
+   },
+   {
+      "name": "Satya Nadella",
+      "company": "Microsoft",
+      "emailid": "satyanadella@microsoft.com",
+      "contactno": "123-456-333",
+      "starting_date": "2012/07/01",
+      "location": "Seattle"
+   },
+   {
+      "name": "Sundar Pichai",
+      "company": "Google",
+      "emailid": "sundarpichai@google.com",
+      "contactno": "123-456-222",
+      "starting_date": "2014/02/01",
+      "location": "Mountain View"
+   },
+   {
+      "name": "Mark Zuckerburg",
+      "company": "Facebook",
+      "emailid": "markzuckerburg@facebook.com",
+      "contactno": "123-456-111",
+      "starting_date": "2013/06/01",
+      "location": "Menlo Park"
+   },
+   {
+      "name": "Reed Hastings",
+      "company": "Netflix",
+      "emailid": "reedhastings@netflix.com",
+      "contactno": "123-456-000",
+      "starting_date": "2014/07/07",
+      "location": "Los Gatos"
+   },
+   {
+      "name": "Dara Khosrowshahi",
+      "company": "Uber",
+      "emailid": "darakhosrowshahi@uber.com",
+      "contactno": "123-999-999",
+      "starting_date": "2017/01/01",
+      "location": "San Fransisco"
+   },
+   {
+      "name": "John Legere",
+      "company": "T-Mobile",
+      "emailid": "johnlegere@tmobile.com",
+      "contactno": "123-999-888",
+      "starting_date": "2012/08/08",
+      "location": "Bellevue"
+   },
+   {
+      "name": "Brad Smith",
+      "company": "Intuit",
+      "emailid": "bradsmith@intuit.com",
+      "contactno": "123-999-777",
+      "starting_date": "2008/07/05",
+      "location": "Mountain View"
+   },
+   {
+      "name": "Ryan Smith",
+      "company": "Qualtrics",
+      "emailid": "ryansmith@qualtrics.com",
+      "contactno": "123-999-666",
+      "starting_date": "2002/01/10",
+      "location": "Provo"
+   },
+   {
+      "name": "Micheal Dell",
+      "company": "Dell",
+      "emailid": "michealdell@dell.com",
+      "contactno": "123-999-555",
+      "starting_date": "2000/04/01",
+      "location": "Round Rock"
+   }
+  ]
+}
